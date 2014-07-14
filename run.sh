c6243c38f27b183a40c25fa11bcfae5bd97d47da
Daniel J. Pritchett <dpritchett@gmail.com>
2014-07-14 14:42:15 -0500

    Purged deploy stuff

--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env sh
-
-export GOPATH=/gopath
-export PATH=$PATH:$GOPATH/bin
-export HOSTNAME=104.131.246.53
-export PORT=80
+export PORT=3999
 
 present -http="0.0.0.0:${PORT}" -orighost="${HOSTNAME}"
